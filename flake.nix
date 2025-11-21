ffb95b445cb5b4210862b57cb66e2adcaa761492
mx6436 <mx6436@foxmail.com>
2025-11-21 17:07:03 +0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    # This is pointing to an unstable release.
+    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
+    # i.e. nixos-24.11
+    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # helix editor, use the master branch
+    helix.url = "github:helix-editor/helix/master";
+  };
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    # NOTE: 'nixos' is the default hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+
+        # 将 home-manager 配置为 nixos 的一个 module
+        # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
+        home-manager.nixosModules.home-manager
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.mx = ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,76 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "mx";
+  home.homeDirectory = "/home/mx";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    fastfetch
+    starship
+    eza
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git.enable = true;
+  programs.git.settings = {
+    user.name = "mx6436";
+    user.email = "mx6436@foxmail.com";
+  };
+
+  # launch fish unless the parent process is already fish
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting
+      alias ls 'eza --group-directories-first'
+      alias ll 'eza -l --group-directories-first'
+      alias la 'eza -la --group-directories-first'
+
+      # muticd
+      function multicd
+          echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+      end
+      abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+      # init starship
+      if status is-interactive && test $XDG_SESSION_TYPE = wayland
+          starship init fish | source
+
+          # enable icons only on wayland
+          alias ls 'eza --group-directories-first --icons=auto'
+          alias ll 'eza -l --group-directories-first --icons=auto'
+          alias la 'eza -la --group-directories-first --icons=auto'
+      end
+    '';
+  };
+
+  programs.helix = {
+    enable = true;
+    settings.theme = "tokyonight_moon";
+  };
+}
