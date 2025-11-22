6a1a562f053495f7ba354d5540d01f6902b3e1fd
mx6436 <mx6436@foxmail.com>
2025-11-22 15:13:24 +0800

    Remove libgcc

--- configuration.nix
+++ configuration.nix
@@ -160,7 +160,7 @@ in
     helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     rustup
-    libgcc
+    gcc
     clash-verge-rev
 
     sbctl # For debugging and troubleshooting Secure Boot.
