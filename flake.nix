1a0c27be0cd3a06afc1b0e5497bab37b08882be5
mx6436 <mx6436@foxmail.com>
2026-01-05 15:51:28 +0800

    flake: Update inputs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
     # i.e. nixos-24.11
     # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # home-manager, used for managing user configuration
     home-manager = {
