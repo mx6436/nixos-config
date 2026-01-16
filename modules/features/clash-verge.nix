e4466c934c25316aca2869e346760c58508d45ce
mx6436 <mx6436@foxmail.com>
2026-01-16 13:55:39 +0800

    fix: Add nftable rules to allow Mihomo through rpfilter

--- modules/features/clash-verge.nix
+++ modules/features/clash-verge.nix
@@ -3,12 +3,15 @@
 {
   programs.clash-verge = {
     enable = true;
+    package = pkgs.clash-verge-rev;
     serviceMode = true;
     tunMode = true;
     autoStart = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    clash-verge-rev
-  ];
+  # See https://github.com/NixOS/nixpkgs/issues/477636
+  networking.firewall = {
+    trustedInterfaces = [ "Mihomo" ];
+    extraReversePathFilterRules = ''iifname { "Mihomo" } accept comment "trusted interface"'';
+  };
 }
