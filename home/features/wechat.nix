abd46a11ba22e158b15ed541828ed070586fc602
mx6436 <mx6436@foxmail.com>
2026-03-16 09:57:07 +0800

    chore(wechat): bump to version 4.1.1.4

--- home/features/wechat.nix
+++ home/features/wechat.nix
@@ -8,10 +8,10 @@
         pkgs.callPackage path (
           args
           // {
-            version = "4.0.1.16";
+            version = "4.1.1.4";
             src = pkgs.fetchurl {
               url = "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.AppImage";
-              hash = "sha256-Pfl81lNVlMJWyPqFli1Af2q8pRLujcKCjYoILCKDx8U=";
+              hash = "sha256-XxAvFnlljqurGPDgRr+DnuCKbdVvgXBPh02DLHY3Oz8=";
             };
           }
         );
