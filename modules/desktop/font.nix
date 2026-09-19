93a91dfd81e0987aa2d492575230768781de06a9
mx6436 <mx6436@foxmail.com>
2026-09-20 07:01:05 +0800

    fonts: add microsoft fonts

--- modules/desktop/font.nix
+++ modules/desktop/font.nix
@@ -9,21 +9,41 @@
     noto-fonts-color-emoji
     maple-mono.NF
     corefonts
+    vista-fonts
+    vista-fonts-chs
   ];
+
+  # 修正简体中文显示为异体（日文）字形
   fonts.fontconfig = {
-    defaultFonts = {
-      serif = [
-        "Noto Serif CJK SC"
-        "Noto Serif"
-      ];
-      sansSerif = [
-        "Noto Sans CJK SC"
-        "Noto Sans"
-      ];
-      monospace = [
-        "Maple Mono NF"
-        "Noto Sans Mono CJK SC"
-      ];
-    };
+    localConf = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+    <fontconfig>
+      <alias>
+        <family>sans-serif</family>
+        <prefer>
+          <family>Noto Sans CJK SC</family>
+          <family>Noto Sans CJK TC</family>
+          <family>Noto Sans CJK JP</family>
+        </prefer>
+      </alias>
+      <alias>
+        <family>serif</family>
+        <prefer>
+          <family>Noto Serif CJK SC</family>
+          <family>Noto Serif CJK TC</family>
+          <family>Noto Serif CJK JP</family>
+        </prefer>
+      </alias>
+      <alias>
+        <family>monospace</family>
+        <prefer>
+          <family>Noto Sans Mono CJK SC</family>
+          <family>Noto Sans Mono CJK TC</family>
+          <family>Noto Sans Mono CJK JP</family>
+        </prefer>
+      </alias>
+    </fontconfig>
+    '';
   };
 }
